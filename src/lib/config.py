af31bc375c5cc56059263fc51023afafa639eba5
Erik Grinaker <erikg@codepoet.no>
2004-11-23 18:02:53 +0000

    moved config handling into separate module, and wrote unit tests for it

---
+++ src/lib/config.py
@@ -0,0 +1,157 @@
+#
+# Revelation 0.3.3 - a password manager for GNOME 2
+# http://oss.codepoet.no/revelation/
+# $Id: data.py 169 2004-10-23 22:27:37Z erikg $
+#
+# Module for configuration handling
+#
+#
+# Copyright (c) 2003-2004 Erik Grinaker
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+
+from revelation import io, util
+import revelation
+
+import gconf
+
+
+class ConfigError(Exception):
+	"Configuration error exception"
+	pass
+
+
+
+class Config(object):
+	"Configuration class"
+
+	def __init__(self, basedir = "/apps/revelation"):
+		self.basedir	= basedir
+		self.client	= gconf.client_get_default()
+		self.callbacks	= {}
+
+		# check config, install schema if needed
+		if self.check() == False:
+			if self.__install_schema(revelation.DIR_GCONFSCHEMAS + "/revelation.schemas") == False or self.check() == False:
+				raise ConfigError
+
+		self.client.add_dir(self.basedir, gconf.CLIENT_PRELOAD_NONE)
+
+
+	def __cb_notify(self, client, id, entry, data):
+		"Callback for handling gconf notifications"
+
+		value = entry.get_value()
+
+		if value.type == gconf.VALUE_STRING:
+			v = value.get_string()
+
+		elif value.type == gconf.VALUE_BOOL:
+			v = value.get_bool()
+
+		elif value.type == gconf.VALUE_INT:
+			v = value.get_int()
+
+
+		# look up and call the callback
+		callback, userdata = self.callbacks[id]
+		callback(entry.get_key(), v, userdata)
+
+
+	def __install_schema(self, file):
+		"Installs a gconf schema"
+
+		if not io.file_exists(file):
+			return False
+
+		util.execute(revelation.FILE_GCONFTOOL + " --install-schema-file=" + file)
+
+
+	def __resolve_keypath(self, key):
+		"Resolves a key path"
+
+		return key[0] == "/" and key or self.basedir + "/" + key
+
+
+	def check(self):
+		"Checks if the config is valid"
+
+		try:
+			self.get("file/autoload_file")
+			self.get("file/autosave")
+			self.get("view/window-position-y")
+			self.get("view/window-width")
+			self.get("launcher/website")
+			self.get("view/searchbar")
+
+		except ConfigError:
+			return False
+
+		else:
+			return True
+
+
+	def get(self, key):
+		"Looks up a config value"
+
+		value = self.client.get(self.__resolve_keypath(key))
+
+		if value is None:
+			raise ConfigError
+
+		elif value.type == gconf.VALUE_STRING:
+			return value.get_string()
+
+		elif value.type == gconf.VALUE_INT:
+			return value.get_int()
+
+		elif value.type == gconf.VALUE_BOOL:
+			return value.get_bool()
+
+
+	def monitor(self, key, callback, userdata = None):
+		"Monitor a config key for changes"
+
+		key = self.__resolve_keypath(key)
+
+		id = self.client.notify_add(key, self.__cb_notify)
+		self.callbacks[id] = ( callback, userdata )
+
+		# call the callback to set an initial state
+		callback(key, self.get(key), userdata)
+
+		return id
+
+
+	def set(self, key, value):
+		"Sets a configuration value"
+
+		node = self.client.get(self.__resolve_keypath(key))
+
+		if node is None:
+			raise ConfigError
+
+		elif node.type == gconf.VALUE_STRING:
+			node.set_string(value)
+
+		elif node.type == gconf.VALUE_BOOL:
+			node.set_bool(value)
+
+		elif node.type == gconf.VALUE_INT:
+			node.set_int(value)
+
+		self.client.set(self.__resolve_keypath(key), node)
+
---
+++ test/config.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+
+#
+# Revelation 0.3.3 - a password manager for GNOME 2
+# http://oss.codepoet.no/revelation/
+# $Id: util.py 174 2004-11-19 16:31:30Z erikg $
+#
+# Unit tests for config module
+#
+#
+# Copyright (c) 2003-2004 Erik Grinaker
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+
+from revelation import config
+
+import gconf, gtk, unittest
+
+
+
+class Config__init(unittest.TestCase):
+	"Config.__init__()"
+
+	def test_basedir(self):
+		"Config.__init__() sets correct default basedir"
+
+		cfg = config.Config()
+		self.assertEquals(cfg.basedir, "/apps/revelation")
+
+
+	def test_inv_basedir(self):
+		"Config.__init__() raises ConfigError on invalid basedir"
+
+		self.assertRaises(config.ConfigError, config.Config, "/test/123")
+
+
+
+class Config_check(unittest.TestCase):
+	"Config.check()"
+
+	pass
+
+
+
+class Config_get(unittest.TestCase):
+	"Config.get()"
+
+
+	def test_absolute(self):
+		"Config.get() handles absolute paths"
+
+		cfg = config.Config()
+		cfg.get("/desktop/gnome/interface/gtk_theme")
+
+
+	def test_nonexist(self):
+		"Config.get() raises ConfigError on non-existant node"
+
+		cfg = config.Config()
+		self.assertRaises(config.ConfigError, cfg.get, "test/123")
+
+
+	def test_relative(self):
+		"Config.get() handles relative paths"
+
+		cfg = config.Config()
+		cfg.get("file/autoload_file")
+
+
+
+class Config_monitor(unittest.TestCase):
+	"Config.monitor()"
+
+	def test_change(self):
+		"Config.monitor() calls callback on change"
+
+		global foo
+		foo = 0
+
+		def cb(key, value, userdata):
+			global foo
+			foo += 1
+
+			if gtk.main_level() > 0:
+				gtk.main_quit()
+
+		cfg = config.Config()
+		cfg.monitor("file/autoload_file", cb)
+
+		cfg.set("file/autoload_file", "test123")
+		gtk.main()
+		self.assertEquals(foo, 2)
+
+
+	def test_init(self):
+		"Config.monitor() calls callback on setup"
+
+		def cb(key, value, userdata):
+			global foo
+			foo = "test"
+
+		cfg = config.Config()
+		cfg.monitor("file/autoload_file", cb)
+
+		self.assertEquals(foo, "test")
+
+
+
+class Config_set(unittest.TestCase):
+	"Config.set()"
+
+	def test_absolute(self):
+		"Config.set() handler absolute paths"
+
+		cfg = config.Config()
+
+		cfg.set("/apps/revelation/file/autoload_file", "123test")
+		self.assertEquals(cfg.get("/apps/revelation/file/autoload_file"), "123test")
+
+
+	def test_relative(self):
+		"Config.set() handles relative paths"
+
+		cfg = config.Config()
+
+		cfg.set("file/autoload_file", "test123")
+		self.assertEquals(cfg.get("file/autoload_file"), "test123")
+
+
+
+
+
+
+
+if __name__ == "__main__":
+	unittest.main()
+
