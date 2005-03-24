0a56346a48790e6079fb1c6c9d6155d2333b5402
Erik Grinaker <erikg@codepoet.no>
2005-03-24 15:08:17 +0000

    minor cleanups of authmanager wrapping code

--- wrap/authmanager/authmanagermodule.c
+++ wrap/authmanager/authmanagermodule.c
@@ -4,19 +4,16 @@
 void authmanager_register_classes (PyObject *d);
 extern PyMethodDef authmanager_functions[];
 
-DL_EXPORT(void)
-initauthmanager(void)
-{
+DL_EXPORT(void) initauthmanager(void) {
 	PyObject *m, *d;
 
-	init_pygobject ();
-	m = Py_InitModule ("authmanager", authmanager_functions);
+	init_pygobject();
+	m = Py_InitModule("authmanager", authmanager_functions);
 	d = PyModule_GetDict(m);
 
-	authmanager_register_classes (d);
+	authmanager_register_classes(d);
 
-	if (PyErr_Occurred ()) {
+	if (PyErr_Occurred())
 		Py_FatalError ("can't initialize module authmanager");
-	}
 }
 
