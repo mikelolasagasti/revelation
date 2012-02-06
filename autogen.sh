5c80436cf391985715d89b3fa2bd560d037fa56f
Tristan Heaven <tristanheaven@gmail.com>
2012-02-06 22:29:38 +0100

    Remove generated files
    Signed-off-by: Mikel Olasagasti Uranga <mikel@olasagasti.info>

--- autogen.sh
+++ autogen.sh
@@ -6,33 +6,13 @@
 # Generates initial makefiles etc
 #
 
-: ${AUTOCONF=autoconf}
-: ${AUTOMAKE=automake}
-: ${ACLOCAL=aclocal}
+[ -f autogen.sh ] || { echo "autogen.sh should be run from srcdir"; exit 1; }
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir="."
+echo "Running glib-gettextize..."
+glib-gettextize --copy --force || { echo "glib-gettextize failed"; exit 1; }
 
-# avoid using caches
-rm -rf autom4te.cache
-rm -f aclocal.m4
+echo "Running intltoolize..."
+intltoolize --automake --copy --force || { echo "intltoolize failed"; exit 1; }
 
-# generates makefiles etc
-echo "Running $ACLOCAL..."
-WANT_AUTOMAKE="1.9" $ACLOCAL || exit 1
-test -f aclocal.m4 || \
-	{ echo "aclocal failed to generate aclocal.m4" 2>&1; exit 1; }
-
-echo "Running $AUTOCONF..."
-WANT_AUTOMAKE="1.9" $AUTOCONF || exit 1
-test -f configure || \
-	{ echo "autoconf failed to generate configure" 2>&1; exit 1; }
-
-echo "Running $AUTOMAKE..."
-WANT_AUTOMAKE="1.9" $AUTOMAKE || exit 1
-test -f Makefile.in || \
-	{ echo "automake failed to generate Makefile.in" 2>&1; exit 1; }
-
-# clean up
-rm -rf autom4te.cache
-
+echo "Running autoreconf..."
+autoreconf --force --install || { echo "autoreconf failed"; exit 1; }
